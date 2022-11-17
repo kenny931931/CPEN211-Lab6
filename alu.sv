cd73bd894a18195f279a4728c29548d4a6fcd345
kenny931931 <112777093+kenny931931@users.noreply.github.com>
2022-11-17 14:15:02 -0800

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -17,14 +17,17 @@ module ALU(input [15:0] val_A, input [15:0] val_B, input [1:0] ALU_op, output [1
 	if (result == 16'b0)
 	  state = 3'b100;
 	// if result is -ve
-	else if (result[15] == 1'b0)
+	else if (result[15] == 1'b1)
 	  state = 3'b010;
-	// if result is overflow (+ve + +ve = -ve OR -ve + -ve = +ve)
-	else if ((ALU_op == 2'b00 && {val_A[15], val_B[15], result[15]} == 3'b001) || (ALU_op == 2'b01 && {val_A[15], val_B[15], result[15]} == 3'b110))
-	  state = 3'b001;
 	else
 	// default case
 	  state = 3'b0;
-	end
+	
+	// if result is overflow (+ve + +ve = -ve OR -ve + -ve = +ve) or (-ve - +ve = +ve OR +ve - -ve = -ve)
+	if (ALU_op == 2'b00 && (({val_A[15], val_B[15], result[15]} == 3'b001) || ({val_A[15], val_B[15], result[15]} == 3'b110)))
+	  state[0] = 3'b1;
+	if (ALU_op == 2'b01 && (({val_A[15], val_B[15], result[15]} == 3'b100) || ({val_A[15], val_B[15], result[15]} == 3'b011)))
+	  state[0] = 3'b1;
+	  
   end
 endmodule: ALU
